e5054c44e9e3fe7f166514abc6bd7eee22279d79
klee-arc <kenfjy@gmail.com>
2016-04-26 18:27:33 +0100

    added gitignore

--- test/js/vibration.js
+++ test/js/vibration.js
@@ -4,6 +4,12 @@ $(function() {
 
 function test() {
   console.log("testing");
-  window.navigator.vibrate(2000); // vibrate for 200ms
+  setInterval(vibrate, 1000);
+}
+
+function vibrate() {
+  var secs = millis | 2000;
+  console.log(secs);
+  window.navigator.vibrate(secs); // vibrate for 200ms
   //window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
 }
