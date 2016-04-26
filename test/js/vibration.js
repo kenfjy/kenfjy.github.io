d4525d2dff9546ebb4a9f8e00afc31664e3125e5
klee-arc <kenfjy@gmail.com>
2016-04-26 18:21:11 +0100

    update time

--- test/js/vibration.js
+++ test/js/vibration.js
@@ -4,6 +4,6 @@ $(function() {
 
 function test() {
   console.log("testing");
-  window.navigator.vibrate(200); // vibrate for 200ms
-  window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
+  window.navigator.vibrate(2000); // vibrate for 200ms
+  //window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
 }
