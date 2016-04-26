e62efb5885cbcc8cb6e18d09a2200d16f6a8216c
klee-arc <kenfjy@gmail.com>
2016-04-26 18:34:43 +0100

    update time

--- test/js/vibration.js
+++ test/js/vibration.js
@@ -2,6 +2,7 @@ $(function() {
   $("body").onload = test();
 })
 
+/* vibrates for 5s every 10s */
 function test() {
   console.log("testing");
   setInterval(vibrate, 10000);
@@ -10,6 +11,6 @@ function test() {
 function vibrate(millis) {
   var secs = millis | 5000;
   console.log(secs);
-  window.navigator.vibrate(secs); // vibrate for 200ms
+  window.navigator.vibrate(secs);
   //window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
 }
