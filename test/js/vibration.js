ade4ba95d47bc63c71b033970eeba0c38b8d4d83
klee-arc <kenfjy@gmail.com>
2016-04-26 17:48:37 +0100

    added initial files

---
+++ test/js/vibration.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("body").onload = test();
+})
+
+function test() {
+  console.log("testing");
+  window.navigator.vibrate(200); // vibrate for 200ms
+  window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
+}
